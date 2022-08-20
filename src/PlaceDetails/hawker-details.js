0cb50ee913baf8b64019ae59fb823fc8dcbda640
annabel <annatae@DESKTOP-N62GGGT.localdomain>
2022-08-20 13:11:11 +0800

    created cards for each hawker center - different API used from the reviewPost feature

---
+++ src/PlaceDetails/hawker-details.js
@@ -0,0 +1,67 @@
+import {
+  Typography,
+  Grid,
+  Box,
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Button,
+} from "@mui/material";
+import React from "react";
+
+const HawkerDetails = ({ details }) => {
+  return (
+    <Card sx={{ maxWidth: 500, marginBottom: 3 }}>
+      <CardMedia
+        component="img"
+        height="300"
+        image={details.photourl}
+        alt={details.name}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5">
+          {details.name}
+        </Typography>
+        <Box display="flex" justifyContent="space-between">
+          <Typography variant="subtitle1">Address</Typography>
+          <Typography variant="subtitle1">{details.address_myenv}</Typography>
+        </Box>
+        <Grid>
+          <Box display="flex" justifyContent="space-between">
+            <Typography variant="subtitle1">Q1 Cleaning Dates</Typography>
+            <Typography variant="subtitle1">
+              {details.q1_cleaningstartdate} to {details.q1_cleaningenddate}
+            </Typography>
+          </Box>
+          <Box display="flex" justifyContent="space-between">
+            <Typography variant="subtitle1">Q2 Cleaning Dates</Typography>
+            <Typography variant="subtitle1">
+              {details.q2_cleaningstartdate} to {details.q2_cleaningenddate}
+            </Typography>
+          </Box>
+          <Box display="flex" justifyContent="space-between">
+            <Typography variant="subtitle1">Q3 Cleaning Dates</Typography>
+            <Typography variant="subtitle1">
+              {details.q3_cleaningstartdate} to {details.q3_cleaningenddate}
+            </Typography>
+          </Box>
+          <Box display="flex" justifyContent="space-between">
+            <Typography variant="subtitle1">Q4 Cleaning Dates</Typography>
+            <Typography variant="subtitle1">
+              {details.q4_cleaningstartdate} to {details.q4_cleaningenddate}
+            </Typography>
+          </Box>
+        </Grid>
+      </CardContent>
+
+      <CardActions>
+        <Button variant="outlined" size="small">
+          Click for reviews
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default HawkerDetails;
